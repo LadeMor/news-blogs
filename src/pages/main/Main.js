52053136635a0221e1de500429ad5b9d01fd3294
Sergiy <sergpyatko@gmail.com>
2024-10-02 13:19:36 +0300

    Added big post section

--- src/pages/main/Main.js
+++ src/pages/main/Main.js
@@ -47,7 +47,21 @@ const Main = () => {
                     </div>
                 </section>
             </Container>
-            
+            <Container width={1440}>
+                <section id="big-post">
+                        <div className="big-post-content">
+                            <p className="tag-label big-post-tag">FASHON</p>
+                            <h1 className="big-post-title">
+                                Richird Norton photorealistic rendering as real photos
+                            </h1>
+                            <p className="big-post-text">
+                            Progressively incentivize cooperative systems through technically sound functionalities. The credibly productivate seamless data.
+                            </p>
+                            <div className="horizontal-line"></div>
+                            <p className="big-post-date">08.08.2021</p>
+                        </div>
+                </section>
+            </Container>
         </>
     );
 }
